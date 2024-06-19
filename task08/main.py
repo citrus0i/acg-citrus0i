6018c023d9288283e8a93bbb3e6cd96b7679f773
zhangyiheng <hi@zhangyihengnoMacBook-Air.local>
2024-06-20 02:35:13 +0900

    task08 finished

--- task08/main.py
+++ task08/main.py
@@ -95,7 +95,7 @@ class HelloWorld(mglw.WindowConfig):
             # below, write one or two lines of code to compute `bone2globalTransformation[i_bone]`
             # hint: use numpy.matmul for multiplying nd-array
             # bone2globalTransformation[i_bone] = ???
-
+            bone2globalTransformation[i_bone] = np.matmul(bone2globalTransformation[i_bone_parent], bone2relativeTransformation[i_bone])
         for i_vtx in range(self.vtx2xyz_ini.shape[0]):  # for each point in mesh
             p0 = self.vtx2xyz_ini[i_vtx]
             p0 = np.append(p0, np.array([1.0]))  # homogeneous coordinate of undeformed point
@@ -108,8 +108,8 @@ class HelloWorld(mglw.WindowConfig):
                 # write a few lines of codes to compute p1 using the linear blend skinning
                 # hint: use np.matmul for matrix multiplication
                 # hint: assume that rig weights w add up to one
-
-                # p1 += ???
+                tmp = np.matmul(w * np.matmul(globalTransformation, inverseBindingMatrix), p0)
+                p1 += tmp
 
             self.vtx2xyz_def[i_vtx] = p1[:3]  # from homogeneous coordinates to the Cartesian coordinates
 
